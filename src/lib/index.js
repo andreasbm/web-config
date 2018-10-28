a6d8b5953badba106015c9dd2b0ac41fd0404fa8
andreasbm <andmehlsen@gmail.com>
2018-10-28 21:51:45 +0100

    Added rollup workbox plugin.

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,7 +3,9 @@ export * from "./rollup-plugins/rollup-plugin-minify-lit-html.js";
 export * from "./rollup-plugins/rollup-plugin-html-template.js";
 export * from "./rollup-plugins/rollup-plugin-livereload.js";
 export * from "./rollup-plugins/rollup-plugin-copy.js";
+export * from "./rollup-plugins/rollup-plugin-workbox.js";
 export * from "./create-rollup-config.js";
 export * from "./create-babel-config.js";
 export * from "./create-karma-config.js";
 export * from "./testing-helpers.js";
+
---
+++ src/lib/rollup-plugins/rollup-plugin-workbox.js
@@ -0,0 +1,56 @@
+import colors from "colors";
+import {generateSW, injectManifest} from "workbox-build";
+
+/**
+ * Default configuration for the workbox rollup plugin.
+ * @type {{mode: string, verbose: boolean}}
+ */
+const defaultConfig = {
+	mode: "generateSW",
+	verbose: true
+};
+
+/**
+ * Returns the correct method to for generating the Service Worker.
+ * @param mode
+ * @returns {*}
+ */
+function workboxFactory (mode) {
+	if (mode === `injectManifest`) {
+		return injectManifest;
+
+	} else if (mode === `generateSW`) {
+		return generateSW;
+	}
+
+	throw new Error(`[workbox] - The mode "${mode} is not valid"`);
+}
+
+/**
+ * A plugin that uses Workbox to generate a Service Worker.
+ * @param config
+ * @returns {{name: string, generateBundle: generateBundle}}
+ */
+export default function workbox (config, ) {
+	const {workboxConfig, mode, verbose} = {...defaultConfig, ...config};
+
+	// Ensure a workbox config exists
+	if (workboxConfig == null) {
+		throw new Error(`[workbox] - The workboxConfig needs to be defined`);
+	}
+
+	return {
+		name: "workbox",
+		generateBundle: async () => {
+			try {
+				await workboxFactory(mode)(workboxConfig);
+
+			} catch (ex) {
+				if (verbose) {
+					console.log(colors.red(`[workbox] - The Service Worker could not be generated: "${ex.message}"`));
+				}
+			}
+		}
+	};
+}
+
